d9a2ba07736aff71081568f962e0574541624e78
webprogramingunpas <2382040@unai.edu>
2026-02-18 15:03:40 +0700

    Menambakan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World<h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi animi assumenda, soluta repudiandae iure consequatur quasi! Porro, cumque officia? Facilis rerum culpa aperiam architecto minus excepturi consequatur earum distinctio iusto!<p>
 </body>
 </html>
